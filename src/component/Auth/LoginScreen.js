7e263d53a30453837c43d9f3ed1d351ae3050972
WSD <wahyu.siddarta97@gmail.com>
2023-11-18 13:23:36 +0800

    update

--- src/component/Auth/LoginScreen.js
+++ src/component/Auth/LoginScreen.js
@@ -22,6 +22,7 @@ import {
 import {MyPrimaryButton} from '../common/Button';
 import {AuthContext} from '../../_context/AuthContext';
 import {isEmpty, validateEmail} from '../../_helpers/utils';
+import {useIsFold} from '../../_hooks';
 
 const LoginScreen = ({navigation}) => {
   const {user, signIn} = useContext(AuthContext);
@@ -34,6 +35,7 @@ const LoginScreen = ({navigation}) => {
   const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [isValid, setIsValid] = useState(false);
+  const {isFold} = useIsFold();
   useEffect(() => {
     let valid = true;
     if (!validateEmail(email)) {
@@ -60,7 +62,7 @@ const LoginScreen = ({navigation}) => {
           backgroundColor={Theme.color.primaryColor}
         />
         <View style={{backgroundColor: Theme.color.primaryColor, height: 30}}>
-          <Svg height={150} width={width} viewBox="0 0 1440 320">
+          <Svg height={isFold ? 200 : 150} width={width} viewBox="0 0 1440 320">
             <Path
               fill={Theme.color.primaryColor}
               d="M0,288L48,261.3C96,235,192,181,288,160C384,139,480,149,576,160C672,171,768,181,864,154.7C960,128,1056,64,1152,53.3C1248,43,1344,85,1392,106.7L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
--- src/component/Auth/RegisterScreen.js
+++ src/component/Auth/RegisterScreen.js
@@ -17,6 +17,7 @@ import {PasswordInput, RegularInput, SafeView} from '../common';
 import {MyPrimaryButton} from '../common/Button';
 import {AuthContext} from '../../_context/AuthContext';
 import {isEmpty} from '../../_helpers/utils';
+import {useIsFold} from '../../_hooks';
 
 const RegisterScreen = ({navigation}) => {
   const {user} = useContext(AuthContext);
@@ -26,6 +27,7 @@ const RegisterScreen = ({navigation}) => {
   const {width, height} = useWindowDimensions();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const {isFold} = useIsFold();
 
   return (
     <SafeView style={{flex: 1}}>
@@ -35,7 +37,7 @@ const RegisterScreen = ({navigation}) => {
           backgroundColor={Theme.color.primaryColor}
         />
         <View style={{backgroundColor: Theme.color.primaryColor, height: 30}}>
-          <Svg height={150} width={width} viewBox="0 0 1440 320">
+          <Svg height={isFold ? 200 : 150} width={width} viewBox="0 0 1440 320">
             <Path
               fill={Theme.color.primaryColor}
               d="M0,288L48,261.3C96,235,192,181,288,160C384,139,480,149,576,160C672,171,768,181,864,154.7C960,128,1056,64,1152,53.3C1248,43,1344,85,1392,106.7L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
